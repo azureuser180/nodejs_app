bf3930d5c06ee18ee19ab933328bc9353158ce1d
Ubuntu <ubuntu@ip-172-31-26-38.ec2.internal>
2023-10-17 03:14:25 +0000

    app modify

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Ya allah Ya mohammand . allah is the best and mohammad sallallaho alywasallam is the best!\n');
+  res.end('Hello, Ya allah Ya mohammand . allah is the best and mohammad sallallaho alywasallam is the best!\n Ya allah Ya mohammad');
 });
 
 const port = process.env.PORT || 3000;
