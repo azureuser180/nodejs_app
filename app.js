ae0f2ef291942d1c985fe27e5ed0abcf949aa386
Ubuntu <ubuntu@ip-172-31-20-202.ec2.internal>
2023-10-21 09:57:10 +0000

    app.js updated1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(' Ya allah Ya rasul');
+  res.end(' Ya allah Ya nabi');
 });
 
 const port = process.env.PORT || 3000;
